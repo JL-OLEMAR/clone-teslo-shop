c7a86eae991ba77a03dfb85f85c6edab37e9f1de
JL-OLEMAR <joseluis19963@gmail.com>
2023-10-05 14:40:20 -0500

    feat: Display cards

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
-import { Typography } from '@mui/material'
+import { Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material'
 import { ShopLayout } from '@/components/layouts'
+import { initialData } from '@/database/products'
 
 export default function Home() {
   return (
@@ -9,6 +10,24 @@ export default function Home() {
     >
       <Typography variant='h1' component='h1'>Store</Typography>
       <Typography variant='h2' sx={{ mb: 1 }}>All products</Typography>
+
+      <Grid container spacing={4}>
+        {
+          initialData.products.map(product => (
+            <Grid item xs={6} sm={4} key={product.slug}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component='img'
+                    image={`products/${product.images[0]}`}
+                    alt={`${product.title} image`}
+                  />
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))
+        }
+      </Grid>
     </ShopLayout>
   )
 }
