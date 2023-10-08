66b95275ccb91ad298ee632e4ffe82f1d593a5b7
JL-OLEMAR <joseluis19963@gmail.com>
2023-10-08 03:23:57 -0500

    feat: Cart page

---
+++ pages/cart/empty.tsx
@@ -0,0 +1,32 @@
+import { Box, Typography } from '@mui/material'
+import { RemoveShoppingCartOutlined } from '@mui/icons-material'
+import { ShopLayout } from '@/components/layouts'
+import Link from 'next/link'
+
+export default function Empty() {
+  return (
+    <ShopLayout title='Empty cart' pageDescription='No items in cart yet. Start shopping now!'>
+      <Box
+        display='flex'
+        justifyContent='center'
+        alignItems='center'
+        height='calc(100vh - 200px)'
+        sx={{ flexDirection: { xs: 'column', sm: 'row' } }}
+      >
+        <RemoveShoppingCartOutlined sx={{ fontSize: 100 }} />
+        <Box display='flex' flexDirection='column' alignItems='center'>
+          <Typography>Your cart is empty!</Typography>
+          <Link href='/' style={{ textDecoration: 'none' }}>
+            <Typography
+              component='span'
+              fontSize={24}
+              fontWeight={500}
+              color='secondary'
+            >Go shopping
+            </Typography>
+          </Link>
+        </Box>
+      </Box>
+    </ShopLayout>
+  )
+}
---
+++ pages/cart/index.tsx
@@ -0,0 +1,31 @@
+import { Box, Button, Card, CardContent, Divider, Grid, Typography } from '@mui/material'
+import { ShopLayout } from '@/components/layouts'
+
+export default function Cart() {
+  return (
+    <ShopLayout title='Cart' pageDescription='Store shopping cart'>
+      <Grid container>
+        <Grid item xs={12} sm={7}>
+          {/* <CartList /> */}
+        </Grid>
+
+        {/* Order */}
+        <Grid item xs={12} sm={5}>
+          <Card className='summary-card'>
+            <CardContent>
+              <Typography variant='h2'>Order Summary</Typography>
+              <Divider sx={{ my: 1 }} />
+
+              {/* <OrderSummary /> */}
+              <Box display='flex' justifyContent='end' sx={{ mt: 3 }}>
+                <Button sx={{ width: '100%' }} className='circular-btn' color='secondary'>
+                  Checkout
+                </Button>
+              </Box>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </ShopLayout>
+  )
+}
