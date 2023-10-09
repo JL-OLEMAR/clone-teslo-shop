4b1f083159e52e9cdad6cbb64fda7cec0c090107
JL-OLEMAR <joseluis19963@gmail.com>
2023-10-09 12:03:04 -0500

    feat: history Order page with DataGrid library

---
+++ pages/orders/history.tsx
@@ -0,0 +1,63 @@
+import Link from 'next/link'
+import { Chip, Grid, Typography } from '@mui/material'
+import { DataGrid, type GridValueGetterParams, type GridColDef } from '@mui/x-data-grid'
+import { ShopLayout } from '@/components/layouts'
+
+const columns: GridColDef[] = [
+  { field: 'id', headerName: 'ID', width: 100 },
+  { field: 'fullname', headerName: 'Full name', width: 250 },
+  {
+    field: 'paid',
+    headerName: 'Paid',
+    description: 'Display the information if the order is paid',
+    width: 200,
+    valueGetter: (params: GridValueGetterParams) => {
+      return (
+        params.row.paid
+          ? <Chip color='success' label='Paid' variant='outlined' />
+          : <Chip color='error' label='Not paid' variant='outlined' />
+      )
+    }
+  },
+  {
+    field: 'order',
+    headerName: 'view order',
+    description: 'Display the information if the order is paid',
+    width: 200,
+    sortable: false,
+    valueGetter: (params: GridValueGetterParams) => {
+      return (
+        <Link href={`/orders/${params.row.id}`}>View order</Link>
+      )
+    }
+  }
+]
+
+const rows = [
+  { id: 1, paid: true, fullname: 'Juan Perez' },
+  { id: 2, paid: true, fullname: 'Maria Lopez' },
+  { id: 3, paid: false, fullname: 'Pedro Sanchez' },
+  { id: 4, paid: true, fullname: 'Luis Garcia' },
+  { id: 5, paid: false, fullname: 'Ana Martinez' },
+  { id: 6, paid: true, fullname: 'Juan Perez' }
+]
+
+export default function History() {
+  return (
+    <ShopLayout title='Orders history' pageDescription='Customer order history'>
+      <Typography variant='h1' component='h1'>History</Typography>
+
+      <Grid container sx={{ mt: 4 }}>
+        <Grid item xs={12} sx={{ height: 650, width: '100%' }}>
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            pageSizeOptions={[10, 10]}
+          // pageSize={10}
+          // rowsPerPageOptions={[10]}
+          />
+        </Grid>
+      </Grid>
+    </ShopLayout>
+  )
+}
